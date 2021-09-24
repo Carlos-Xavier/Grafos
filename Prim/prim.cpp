3711e0f8c624079eaa1de745a953b294a5138edd
Carlos Eduardo X. Bezerra <47821159+Carlos-Xavier@users.noreply.github.com>
2021-09-24 19:24:32 -0300

    Add files via upload

--- Prim/prim.cpp
+++ Prim/prim.cpp
@@ -2,7 +2,11 @@
 #include <vector>
 #include <queue>
 #include <functional>
+#include <fstream>
 #include <utility>
+#include <bits/stdc++.h>
+
+
 #define MAX 100
 
 using namespace std;
@@ -36,18 +40,102 @@ int  primAlgorithm(int x)
     return minimumCost;
 }
 
-int main()
+bool readGraphByFile(string file)
+{
+    int i = 1;
+    string line;
+    ifstream myfile;
+    int nv, ne, src, dist,wt;
+
+    myfile.open(file);
+    if (!myfile.is_open())
+    {
+        perror("Error open");
+        exit(EXIT_FAILURE);
+    }
+    if ( myfile.is_open() ) {
+        getline(myfile,line);
+        stringstream ss(line);
+        ss >> nv >> ne;
+
+        while (getline(myfile, line))
+        {
+            stringstream ss(line);
+
+            if (ss.str().length() > 4)
+                ss >> src >> dist >> wt;
+            else
+            {
+                ss >> src >> dist;
+                wt = 1;
+            }
+
+            adj[src].push_back(make_pair(wt, dist));
+            adj[dist].push_back(make_pair(wt, src));
+        }
+    }
+
+    return true;
+}
+
+
+bool readGraph()
 {
     int nv, ne, src, dist,wt, minimumCost;
+    char character;
+    bool flag = true;
+
+    cout << "Insert vertices and edges: \n";
     cin >> nv >> ne;
-    for(int i = 0;i < ne;++i)
+
+    cout << "Do you want to include the weights? (y/n) \n";
+    cin >> character;
+
+    if (character == 'y')
     {
-        cin >> src >> dist >> wt;
-        adj[src].push_back(make_pair(wt, dist));
-        adj[dist].push_back(make_pair(wt, src));
+        cout << "Enter the pair of vertices and the weights (Example: x y w): \n";
+        for(int i = 0;i < ne;++i)
+        {
+            cin >> src >> dist >> wt;
+            adj[src].push_back(make_pair(wt, dist));
+            adj[dist].push_back(make_pair(wt, src));
+        }
     }
-    /*Calcula o custo da AGM com o grafo de entrada "arquivo-entrada.dat" e vértice inicial 1.*/
-    minimumCost = primAlgorithm(1);
-    cout << "Custo minimo " << minimumCost << endl;
+    else if (character == 'n')
+    {
+        cout << "Enter the pair of vertices (Example: x y): \n";
+        for(int i = 0;i < ne;++i)
+        {
+            cin >> src >> dist;
+            adj[src].push_back(make_pair(1, dist));
+            adj[dist].push_back(make_pair(1, src));
+        }
+    }
+    else
+    {
+        cout << "Invalid Input \n";
+        flag = false;
+    }
+
+    return flag;
+}
+
+
+int main(int argc, char *argv[])
+{
+    int minimumCost;
+    bool flag;
+
+    if (argc > 1 && string(argv[1]) == "-f")
+        flag = readGraphByFile(argv[2]);
+    else
+        flag = readGraph();
+    
+    if (flag)
+    {
+        /*Calcula o custo da AGM com o grafo de entrada "arquivo-entrada.dat" e vértice inicial 1.*/
+        minimumCost = primAlgorithm(1);
+        cout << "Minimum cost " << minimumCost << endl;
+    }
     return 0;
 }
--- bellman_ford/bellman.cpp
+++ bellman_ford/bellman.cpp
@@ -9,110 +9,113 @@ using namespace std;
 
 typedef struct edge
 {
-  int src;
-  int dest;
-  int wt;
-}edge;
+    int src;
+    int dest;
+    int wt;
+} edge;
 
 void print(int nv,int dis[MAX])
 {
-   cout<<"\nVertice "<<"  Distancia da fonte";
-  for(int i=1;i<=nv;i++)
-  {
-    cout<<"\n"<<i<<"\t"<<dis[i];
-  }
-  cout << "\n";
+    cout<<"\nVertice "<<"  Distancia da fonte";
+    for(int i=1;i<=nv;i++)
+    {
+        cout<<"\n"<<i<<"\t"<<dis[i];
+    }
+    cout << "\n";
 }
 
 void bellman_ford(int nv,edge e[],int src,int ne)
 {
-  int u,v,weight;
-  int dis[MAX];
+    int u,v,weight;
+    int dis[MAX];
 
-  for(int i = 1;i <= nv;i++)
-  {
-    dis[i]= INF;
-  }
-
-  dis[src] = 0;
-
-  for(int i = 1 ;i <= nv - 1; i++)
-  {
-    for(int j = 0;j < ne;j++)
+    for(int i = 1;i <= nv;i++)
     {
-      u=e[j].src;
-      v=e[j].dest;
-      weight=e[j].wt;
-    
-      if(dis[u] != INF && dis[u] + weight < dis[v])
-      {
-        dis[v] = dis[u] + weight;
-      }  
+        dis[i]= INF;
     }
-    
-  }
-  
-  for(int j=0;j < ne;j++)
-  {
-    u=e[j].src;
-    v=e[j].dest;
-    weight=e[j].wt;
-    
-    if(dis[u] != MAX && dis[u] + weight < dis[v])
+
+    dis[src] = 0;
+
+    for(int i = 1 ;i <= nv - 1; i++)
     {
-      cout << "Contém ciclo negativo";
-      return;
-    }  
-  }
-  /*Exibindo a distância do vertice 1 aos demais*/
+        for(int j = 0;j < ne;j++)
+        {
+            u=e[j].src;
+            v=e[j].dest;
+            weight=e[j].wt;
+            
+            if(dis[u] != INF && dis[u] + weight < dis[v])
+            {
+                dis[v] = dis[u] + weight;
+            }  
+        }
+    
+    }
+  
+    for(int j=0;j < ne;j++)
+    {
+        u=e[j].src;
+        v=e[j].dest;
+        weight=e[j].wt;
+        
+        if(dis[u] != MAX && dis[u] + weight < dis[v])
+        {
+            cout << "Contém ciclo negativo";
+            return;
+        }  
+    }
+
+    /*Exibindo a distância do vertice 1 aos demais*/
     print(nv,dis);
 }
 
 void readGraphByFile(string file)
 {
-  edge e[MAX];
-  ifstream myfile;
-  int nv,ne,wt,src,edges,dest;
-  string line;
-  cout << file << '\n';
-  
-  myfile.open(file);
-  if (!myfile.is_open())
+    edge e[MAX];
+    ifstream myfile;
+    int nv,ne,wt,src,edges,dest;
+    string line;
+    cout << file << '\n';
+    
+    myfile.open(file);
+    if (!myfile.is_open())
     {
         perror("Error open");
         exit(EXIT_FAILURE);
     }
 
-  if ( myfile.is_open() ) {
-    getline(myfile,line);
-    stringstream ss(line);
-    ss >> nv >> ne;
-    edges = 0;
-    while (getline(myfile,line))  {
-      int x, y, w;
-      stringstream ss(line);
+    if ( myfile.is_open() ) 
+    {
+        getline(myfile,line);
+        stringstream ss(line);
+        ss >> nv >> ne;
+        edges = 0;
+        while (getline(myfile,line))  
+        {
+            int x, y, w;
+            stringstream ss(line);
 
-      if (ss.str().length() > 4)
-        ss >> x >> y >> w;
-      else
-      {
-        ss >> x >> y;
-        w = 1;
-      }
+            if (ss.str().length() > 4)
+                ss >> x >> y >> w;
+            else
+            {
+                ss >> x >> y;
+                w = 1;
+            }
 
-      e[edges].src = static_cast<int> (x);
-      e[edges].dest = static_cast<int> (y);
-      e[edges].wt  = static_cast<int> (w);
-    
-      edges++;
-  }
+            e[edges].src = static_cast<int> (x);
+            e[edges].dest = static_cast<int> (y);
+            e[edges].wt  = static_cast<int> (w);
+            
+            edges++;
+        }
 
-   myfile.close();
-  }
+        myfile.close();
+    }
 
-  /*Analisando a distância do vertice 1 aos demais*/
-  int source = 1;
-  bellman_ford(nv,e,source,ne);
+    /*Analisando a distância do vertice 1 aos demais*/
+    int source = 1;
+    bellman_ford(nv,e,source,ne);
 }
 
 void readGraph()
@@ -130,29 +133,29 @@ void readGraph()
 
     if (character == 'y')
     {
-      cout << "Enter the pair of vertices and the weights (Example: x y w): \n";
-      for (int i = 0; i < ne; i++)
-      {
-        int x, y, w;
-        cin >> x >> y >> w;
+        cout << "Enter the pair of vertices and the weights (Example: x y w): \n";
+        for (int i = 0; i < ne; i++)
+        {
+            int x, y, w;
+            cin >> x >> y >> w;
 
-        e[i].src = static_cast<int> (x);
-        e[i].dest = static_cast<int> (y);
-        e[i].wt  = static_cast<int> (w);
-      }
+            e[i].src = static_cast<int> (x);
+            e[i].dest = static_cast<int> (y);
+            e[i].wt  = static_cast<int> (w);
+        }
     }
     else if (character == 'n')
     {
-      cout << "Enter the pair of vertices (Example: x y): \n";
-      for (int i = 0; i < ne; i++)
-      {
-        int x, y;
-        cin >> x >> y;
+        cout << "Enter the pair of vertices (Example: x y): \n";
+        for (int i = 0; i < ne; i++)
+        {
+            int x, y;
+            cin >> x >> y;
 
-        e[i].src = static_cast<int> (x);
-        e[i].dest = static_cast<int> (y);
-        e[i].wt  = static_cast<int> (1);
-      }
+            e[i].src = static_cast<int> (x);
+            e[i].dest = static_cast<int> (y);
+            e[i].wt  = static_cast<int> (1);
+        }
     }
     else
     {
@@ -162,17 +165,17 @@ void readGraph()
 
     if (flag)
     {  
-      /*Analisando a distância do vertice 1 aos demais*/
-      int source = 1;
-      bellman_ford(nv,e,source,ne);
+        /*Analisando a distância do vertice 1 aos demais*/
+        int source = 1;
+        bellman_ford(nv,e,source,ne);
     }
 }
 
 int main(int argc, char *argv[])
 {
-  if (argc > 1 && string(argv[1]) == "-f")
-    readGraphByFile(argv[2]);
-  else
-    readGraph();
-  return 0;
+    if (argc > 1 && string(argv[1]) == "-f")
+        readGraphByFile(argv[2]);
+    else
+        readGraph();
+    return 0;
 }
